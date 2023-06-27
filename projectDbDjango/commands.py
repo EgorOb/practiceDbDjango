efb1e716155a34de021a42cf1b407d65022c0844
Egor <egorobidin@inbox.ru>
2023-06-27 16:33:02 +0300

    сделана часть примеров

--- projectDbDjango/commands.py
+++ projectDbDjango/commands.py
@@ -7,20 +7,7 @@ django.setup()
 if __name__ == "__main__":
     from db.models import Blog, Author, AuthorProfile, Entry
     """Проверьте тут код если необходимо"""
-    from django.db.models import Count
-    blogs = Blog.objects.alias(entries=Count('entry')).filter(entries__gt=4)
-    print(blogs)
-    """
-    <QuerySet [
-    <Blog: Путешествия по миру>, 
-    <Blog: Кулинарные искушения>, 
-    <Blog: Фитнес и здоровый образ жизни>, 
-    <Blog: ИТ-новости и технологии>, 
-    <Blog: Мода и стиль>
-    ]>
-    """
-    ## Выведет ошибку, так как поле entries не существует, виду различий между alias и annotate
-    # blogs = Blog.objects.alias(entries=Count('entry')).filter(entries__gt=4).values('blog', 'entries')
+
 
 
 
