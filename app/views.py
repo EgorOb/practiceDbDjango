4b6eb79b8fdd0d8a9f68ef1db286b11ae4219c2f
Egor <egorobidin@inbox.ru>
2024-01-16 17:36:31 +0300

    Обновление материала

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'app/index.html')
+
--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static  # Чтобы подгрузить о
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include("app.urls")),
+    path('', include('app.urls')),
 ]
 
 if settings.DEBUG:
